cca8ed80072802acdd79a9db574d71deb33864c6
Hung Le <isekaihaibara620@gmail.com>
2026-09-16 12:36:04 +0700

    Cập nhật file API Controller và đổi tên index.html

--- LAB_03/src/main/java/com/example/schoolmanager/controller/StudentRestController.java
+++ LAB_03/src/main/java/com/example/schoolmanager/controller/StudentRestController.java
@@ -1,17 +1,29 @@
 package com.example.schoolmanager.controller;
 
-import com.example.schoolmanager.entity.Student;
-import com.example.schoolmanager.service.StudentService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import com.example.schoolmanager.entity.Student;
+import com.example.schoolmanager.service.StudentService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/students")
